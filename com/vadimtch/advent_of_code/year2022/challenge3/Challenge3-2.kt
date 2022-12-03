cd8b39d4ec561b07f9ffe508b9fb4ec92374f981
Vadim Chichikalyuk <chichikalyuk@gmail.com>
2022-12-03 23:44:47 +0000

    Challenge 3-2

---
+++ com/vadimtch/advent_of_code/year2022/challenge3/Challenge3-2.kt
@@ -0,0 +1,42 @@
+package com.vadimtch.advent_of_code.year2022.challenge3
+
+import java.io.File
+
+fun main() {
+    val file = File("com/vadimtch/advent_of_code/year2022/challenge3/Challenge3.txt")
+    val priorityCalculator = PriorityCalculator()
+    val reader = file.bufferedReader()
+
+    var line = reader.readLine()
+    var total = 0
+    do {
+        val items = HashMap<Char, Int>()
+
+        // counts characters multiple times per line
+        // record
+        repeat(3) {
+            val itemsFound = HashSet<Char>()
+
+            for (char in line) {
+                if (itemsFound.contains(char)) {
+                    continue
+                }
+
+                if (items[char] == null) {
+                    items[char] = 1
+                    itemsFound.add(char)
+                } else if (items[char] == 1) {
+                    items[char] = 2
+                    itemsFound.add(char)
+                } else if (items[char] == 2) {
+                    total += priorityCalculator.getPriority(char)
+                    itemsFound.add(char)
+                }
+            }
+
+            line = reader.readLine()
+        }
+    } while(line != null)
+
+    print(total)
+}
